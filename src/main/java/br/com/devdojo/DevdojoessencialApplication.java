0aabebdc14b8964971ebc178a17c99625d90766d
aevilesaguiar <aeviles@hotmail.com>
2022-11-30 14:11:50 -0300

    Initial commit

---
+++ src/main/java/br/com/devdojo/DevdojoessencialApplication.java
@@ -0,0 +1,13 @@
+package br.com.devdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdojoessencialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdojoessencialApplication.class, args);
+	}
+
+}
